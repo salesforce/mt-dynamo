aae37cb2ec8a99b144e48aab35a2fa95b946b29b
msgroi@salesforce.com <35938980+msgroi@users.noreply.github.com>
2018-06-25 15:25:18 -0700

    updated docs to explain why not all byte array types in SharedTable impl

--- src/main/java/com/salesforce/dynamodbv2/mt/mappers/sharedtable/SharedTableBuilder.java
+++ src/main/java/com/salesforce/dynamodbv2/mt/mappers/sharedtable/SharedTableBuilder.java
@@ -87,6 +87,10 @@ import static java.util.Optional.of;
  * - Virtual tables with only a HK may not be mapped to a table that has both a HK and RK per
  *   https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_PutItem.html, "When you add an item, the primary
  *   key attribute(s) are the only required attributes."
+ * - All virtual types could have been mapped into a set of tables that have only byte array types, and convert
+ *   all virtual types down to byte arrays and back.  This would necessitate a smaller set of tables, possibly as few
+ *   as 3.  However, the mapping layer would also need to be responsible for maintaining consistency with respect to
+ *   sorting so it was not implemented.
  */
 public class SharedTableBuilder extends SharedTableCustomDynamicBuilder {
 
