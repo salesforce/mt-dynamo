5ac42994b46d10a7ec64ca9eb9d43cb318e5c3d6
msgroi@salesforce.com <35938980+msgroi@users.noreply.github.com>
2019-04-04 13:52:49 -0700

    updated test

--- src/test/java/com/salesforce/dynamodbv2/mt/mappers/sharedtable/impl/ItemMapperTest.java
+++ src/test/java/com/salesforce/dynamodbv2/mt/mappers/sharedtable/impl/ItemMapperTest.java
@@ -7,24 +7,16 @@
 
 package com.salesforce.dynamodbv2.mt.mappers.sharedtable.impl;
 
-import static com.amazonaws.services.dynamodbv2.model.ScalarAttributeType.N;
 import static com.amazonaws.services.dynamodbv2.model.ScalarAttributeType.S;
-import static com.salesforce.dynamodbv2.mt.mappers.sharedtable.impl.FieldMapping.IndexType.SECONDARYINDEX;
-import static com.salesforce.dynamodbv2.mt.mappers.sharedtable.impl.FieldMapping.IndexType.TABLE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.salesforce.dynamodbv2.mt.mappers.CreateTableRequestBuilder;
 import com.salesforce.dynamodbv2.mt.mappers.index.DynamoSecondaryIndexMapperByTypeImpl;
 import com.salesforce.dynamodbv2.mt.mappers.metadata.DynamoTableDescriptionImpl;
-import com.salesforce.dynamodbv2.mt.mappers.sharedtable.impl.FieldMapping.Field;
-
 import java.util.Map;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -32,7 +24,6 @@ import org.junit.jupiter.api.Test;
  *
  * @author msgroi
  */
-// TODO msgroi add coverage
 class ItemMapperTest {
 
     private static final String PREFIX = "PREFIX-";
@@ -40,10 +31,11 @@ class ItemMapperTest {
     private static final ItemMapper SUT = new ItemMapper(
             new MockFieldMapper(),
             new TableMapping(new DynamoTableDescriptionImpl(
-                    CreateTableRequestBuilder.builder().withTableKeySchema("virtualhk", S).build()),
+                    CreateTableRequestBuilder.builder()
+                        .withTableKeySchema("virtualhk", S, "virtualrk", S).build()),
                     new SingletonCreateTableRequestFactory(new DynamoTableDescriptionImpl(
                             CreateTableRequestBuilder.builder()
-                                    .withTableKeySchema("physicalhk", S).build())
+                                    .withTableKeySchema("physicalhk", S, "physicalrk", S).build())
                             .getCreateTableRequest()),
                     new DynamoSecondaryIndexMapperByTypeImpl(),
                     null,
@@ -54,53 +46,19 @@ class ItemMapperTest {
     void applyAndReverse() {
         Map<String, AttributeValue> item = ImmutableMap.of(
             "virtualhk", new AttributeValue().withS("hkvalue"),
+            "virtualrk", new AttributeValue().withS("rkvalue"),
             "somefield", new AttributeValue().withS("somevalue"));
 
         Map<String, AttributeValue> mappedItem = SUT.apply(item);
 
         assertEquals(ImmutableMap.of(
             "physicalhk", new AttributeValue().withS(PREFIX + "hkvalue"),
+            "physicalrk", new AttributeValue().withS("rkvalue"),
             "somefield", new AttributeValue().withS("somevalue")), mappedItem);
 
         Map<String, AttributeValue> reversedItem = SUT.reverse(mappedItem);
 
         assertEquals(item, reversedItem);
-    }
-
-    @Test
-    @Disabled
-    // TODO msgroi test
-    void getAllPhysicalToVirtualFieldMappings() {
-        assertEquals(ImmutableMap.of(
-                "physicalhk", ImmutableList.of(new FieldMapping(
-                        new FieldMapping.Field("physicalhk", S),
-                        new Field("virtualhk", N),
-                        "virtualTableName",
-                        "physicalTableName",
-                        TABLE,
-                        true)),
-                "physicalrk", ImmutableList.of(new FieldMapping(
-                        new Field("physicalrk", N),
-                        new Field("virtualrk", N),
-                        "virtualTableName",
-                        "physicalTableName",
-                        TABLE,
-                        false)),
-                "physicalgsihk", ImmutableList.of(new FieldMapping(new Field("physicalgsihk", S),
-                        new Field("virtualgsihk", S),
-                        "virtualgsi",
-                        "physicalgsi",
-                        SECONDARYINDEX,
-                        true)),
-                "physicalgsirk", ImmutableList.of(new FieldMapping(new Field("physicalgsirk", N),
-                        new Field("virtualgsirk", N),
-                        "virtualgsi",
-                        "physicalgsi",
-                        SECONDARYINDEX,
-                        false))),
-                //SUT.getAllPhysicalToVirtualFieldMappings()
-                null
-        );
     }
 
     @Test
