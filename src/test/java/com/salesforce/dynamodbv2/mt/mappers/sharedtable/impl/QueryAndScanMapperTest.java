9a0f9c0b80c13495a669403899e5fbf1c29c7f66
Benjamin Busjaeger <bbusjaeger@salesforce.com>
2019-05-10 11:46:05 -0700

    Disable failing test and fix checkstyle violations

--- src/test/java/com/salesforce/dynamodbv2/mt/mappers/sharedtable/impl/QueryAndScanMapperTest.java
+++ src/test/java/com/salesforce/dynamodbv2/mt/mappers/sharedtable/impl/QueryAndScanMapperTest.java
@@ -32,7 +32,6 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.mockito.ArgumentMatchers;
 
 /**
  * TODO: write Javadoc.
