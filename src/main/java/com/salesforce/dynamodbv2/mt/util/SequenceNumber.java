f99c9f495879ac58e4fb4ca5dfc3fa7527321599
Benjamin Busjaeger <bbusjaeger@salesforce.com>
2019-06-19 03:34:23 -0700

    Rework streams cache

---
+++ src/main/java/com/salesforce/dynamodbv2/mt/util/SequenceNumber.java
@@ -0,0 +1,56 @@
+package com.salesforce.dynamodbv2.mt.util;
+
+import com.amazonaws.services.dynamodbv2.model.Record;
+import java.math.BigInteger;
+import java.util.Objects;
+
+final class SequenceNumber implements Comparable<SequenceNumber> {
+
+    static SequenceNumber fromRawValue(String sequenceNumber) {
+        try {
+            return new SequenceNumber(new BigInteger(sequenceNumber));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    static SequenceNumber fromRecord(Record record) {
+        return fromRawValue(record.getDynamodb().getSequenceNumber());
+    }
+
+    private final BigInteger value;
+
+    private SequenceNumber(BigInteger value) {
+        this.value = value;
+    }
+
+    boolean precedes(SequenceNumber other) {
+        return compareTo(other) <= 0;
+    }
+
+    SequenceNumber next() {
+        return new SequenceNumber(value.add(BigInteger.ONE));
+    }
+
+    @Override
+    public int compareTo(SequenceNumber o) {
+        return value.compareTo(o.value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SequenceNumber that = (SequenceNumber) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
